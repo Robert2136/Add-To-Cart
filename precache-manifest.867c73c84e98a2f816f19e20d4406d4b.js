f83220dda40b6b4826a509396f88d7a817770317
Loboda Robert <loboda_robert@yahoo.com>
2023-10-01 23:38:51 +0300

    Updates

---
+++ precache-manifest.867c73c84e98a2f816f19e20d4406d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b57b93fc3573ec7b7741",
+    "url": "/add-to-cart/static/css/main.b58fea50.chunk.css"
+  },
+  {
+    "revision": "b57b93fc3573ec7b7741",
+    "url": "/add-to-cart/static/js/main.b57b93fc.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/add-to-cart/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "2e03794240033e8340b7",
+    "url": "/add-to-cart/static/js/2.2e037942.chunk.js"
+  },
+  {
+    "revision": "d8b030ec64825783b792",
+    "url": "/add-to-cart/static/js/runtime~main.d8b030ec.js"
+  },
+  {
+    "revision": "a680b2ea02e76b07d7b10afa1cd94259",
+    "url": "/add-to-cart/index.html"
+  }
+];
